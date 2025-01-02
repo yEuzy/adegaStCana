73f8f2fb77d79dd2b99a759a5a561a42167b65a8
yEuzy <kallewfelip100@gmail.com>
2025-01-01 23:23:25 -0300

    arrumando as options da sidebar

--- inicio/php/scripts/sidebropen.php
+++ inicio/php/scripts/sidebropen.php
@@ -2,44 +2,79 @@
 if(!isset($_SESSION)){
     session_start();
 }
-
-echo '
-<div id="sideb">
-        <div id="imgdiv">
-             <h2>' .  $_SESSION["nome"]    .'</h2><br>
-            <img id="profimg" src="" alt="Foto do usuario">
+if(isset($_SESSION['adm'])){
+    echo '
+    <div id="sideb">
+            <div id="imgdiv">
+                 <h2>' .  $_SESSION["nome"]    .'</h2><br>
+                <img id="profimg" src="" alt="Foto do usuario">
+            </div>
+            <div id="options">
+            
+                <a href="./inicio.php">
+                    <button>Inicio</button>
+                </a>
+                <a href="./cadProd.php">
+                    <button>Cadastrar Produtos</button>
+                </a>
+                <a href="./vendas.php">
+                    <button>Listar Vendas</button>
+                </a>
+                <a href="usuarios.php">
+                    <button>Listar Usuarios</button>
+                </a>
+            </div>
         </div>
-        <div id="options">
-        
-            <a href="./inicio.php">
-                <button>Inicio</button>
-            </a>
-            <a href="./cadProd.php">
-                <button>Cadastrar Produtos</button>
-            </a>
-            <a href="./vendas.php">
-                <button>Listar Vendas</button>
-            </a>
-            <a href="usuarios.php">
-                <button>Listar Usuarios</button>
-            </a>
-        </div>
-    </div>
-
-<script> let botao = document.getElementById("sidebutt")
-            let sidsts = false
-            botao.addEventListener("click", function(e){
-                if(!sidsts){
-                    let sidebar = document.getElementById("sideb");
-                    sidebar.style.left = "0%";
-                    botao.innerText = "X";
-                    sidsts = true;
-                }else{
-                    let sidebar = document.getElementById("sideb");
-                    sidebar.style.left = "-30%";
-                    botao.innerText = "≡";
-                    sidsts = false;
+    
+    <script> let botao = document.getElementById("sidebutt")
+                let sidsts = false
+                botao.addEventListener("click", function(e){
+                    if(!sidsts){
+                        let sidebar = document.getElementById("sideb");
+                        sidebar.style.left = "0%";
+                        botao.innerText = "X";
+                        sidsts = true;
+                    }else{
+                        let sidebar = document.getElementById("sideb");
+                        sidebar.style.left = "-30%";
+                        botao.innerText = "≡";
+                        sidsts = false;
+                    }
+    
                 }
-
-            }
-)</script>';?>
+    )</script>';
+    
+}else{
+    echo '
+    <div id="sideb">
+            <div id="imgdiv">
+                 <h2>' .  $_SESSION["nome"]    .'</h2><br>
+                <img id="profimg" src="" alt="Foto do usuario">
+            </div>
+            <div id="options">
+            
+                <a href="./inicio.php">
+                    <button>Inicio</button>
+                </a>
+            </div>
+        </div>
+    
+    <script> let botao = document.getElementById("sidebutt")
+                let sidsts = false
+                botao.addEventListener("click", function(e){
+                    if(!sidsts){
+                        let sidebar = document.getElementById("sideb");
+                        sidebar.style.left = "0%";
+                        botao.innerText = "X";
+                        sidsts = true;
+                    }else{
+                        let sidebar = document.getElementById("sideb");
+                        sidebar.style.left = "-30%";
+                        botao.innerText = "≡";
+                        sidsts = false;
+                    }
+    
+                }
+    )</script>';
+}
+?>
